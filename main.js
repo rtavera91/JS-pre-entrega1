534122e8b6a851d289f1963d64feeae47ba19006
rtavera91 <rtavera91@gmail.com>
2023-04-14 22:48:55 -0700

    Seteo de github

--- main.js
+++ main.js
@@ -1,57 +1,50 @@
-// Pedimos al usuario su edad, edad de retiro y monto de ahorro mensual
-let nombreUsuario = prompt("¿Cuál es tu nombre?");
-let edad = prompt("¿Cuál es tu edad?");
+let ahorros = [];
+
+function AhorroAnual(anio, cantidad) {
+  this.anio = anio;
+  this.cantidad = cantidad;
+}
+
+let edad = prompt("¿Cuál es tu edad actual?");
 let edadRetiro = prompt("¿A qué edad te gustaría retirarte?");
-let ahorroMensual = prompt("¿Cuánto dinero te gustaría ahorrar mensualmente?");
+let ahorroMensual = prompt("¿Cuánto dinero te gustaría ahorrar al mes?");
+let ahorroAnual = ahorroMensual * 12;
 
-// Verificamos si los valores ingresados son números
-if (isNaN(edad) || isNaN(edadRetiro) || isNaN(ahorroMensual)) {
-  console.log(
-    "Error: Debes ingresar solo números. Por favor, intenta de nuevo."
-  );
+let tasaInteres = confirm("¿Quieres usar una tasa de interés promedio del 10%? Presiona 'Aceptar' para sí, y 'Cancelar' para no.");
+if (!tasaInteres) {
+  let porcentaje = prompt("Ingresa la tasa de interés que te gustaría usar en porcentaje (sin el signo %).");
+  while (isNaN(parseFloat(porcentaje))) {
+    alert("Debes ingresar un número válido.");
+    porcentaje = prompt("Ingresa la tasa de interés que te gustaría usar en porcentaje (sin el signo %).");
+  }
+  tasaInteres = parseFloat(porcentaje) / 100;
 } else {
-  // Convertimos las edades y el ahorro mensual a números enteros
-  edad = parseInt(edad);
-  edadRetiro = parseInt(edadRetiro);
-  ahorroMensual = parseInt(ahorroMensual);
-  let ahorroAnual = ahorroMensual * 12;
+  tasaInteres = 0.1; // 10%
+}
 
-    // Preguntamos al usuario si desea utilizar el interés promedio del 10%
-    const usarInteresPromedio = confirm("¿Deseas utilizar el interés promedio anual del 10%?");
-    let interesPorcentaje;
-  
-    // Si el usuario rechaza el interés promedio, le pedimos que ingrese su propio porcentaje
-    if (!usarInteresPromedio) {
-      do {
-        const porcentaje = prompt("Ingresa tu propio porcentaje de interés (sin el signo de porcentaje)");
-        interesPorcentaje = parseFloat(porcentaje);
-      } while (isNaN(interesPorcentaje) || interesPorcentaje <= 0);
-    } else {
-      interesPorcentaje = 10;
-    }
-    // Convertimos el porcentaje de interés a una tasa decimal
-    const interesDecimal = interesPorcentaje / 100;
+let anios = edadRetiro - edad;
+let totalAhorro = 0;
+for (let i = 0; i < anios; i++) {
+  let cantidad = totalAhorro + ahorroAnual;
+  let interes = cantidad * tasaInteres;
+  totalAhorro = cantidad + interes;
+  let anio = new Date().getFullYear() + i;
+  let ahorroActual = new AhorroAnual(anio, totalAhorro);
+  ahorros.push(ahorroActual);
+}
 
-  // Calculamos el número de años que el usuario deberá ahorrar
-  const numAnios = edadRetiro - edad;
+let opcion = prompt("¿Quieres ver el desgloce total o un año en particular? Ingresa 'total' o el año que te gustaría ver (por ejemplo: '2025').");
 
-  // Imprimimos un mensaje con el número de años que el usuario deberá ahorrar
-  console.log(`Necesitas ahorrar por ${numAnios} años hasta tu retiro`);
-  console.log(
-    `${nombreUsuario}, tu ahorro mensual es de $${ahorroMensual} pesos, por lo que tu ahorro anual sería de $${ahorroAnual} pesos`
-  );
-
-  // Creamos una variable para almacenar el total de ahorro
-  let totalAhorro = 0;
-
-  // Creamos un bucle que sume el ahorro anual al total de ahorro por el número de años que se debe ahorrar
-  for (let i = 0; i < numAnios; i++) {
-    totalAhorro += ahorroAnual;
-
-    // Calculamos el interés compuesto del año actual
-    const interesCompuesto = totalAhorro * interesDecimal;
-    totalAhorro += interesCompuesto;
-
-    console.log(`Ahorro del año ${i+1}: $${totalAhorro.toFixed(2)}`);
+if (opcion.toLowerCase() === "total") {
+  console.log("Desgloce total de ahorro:");
+  console.log(ahorros);
+} else {
+  let anioElegido = parseInt(opcion);
+  let ahorrosFiltrados = ahorros.filter(ahorro => ahorro.anio === anioElegido);
+  if (ahorrosFiltrados.length > 0) {
+    console.log(`Ahorro acumulado en el año ${anioElegido}: ${ahorrosFiltrados[0].cantidad}`);
+  } else {
+    console.log(`No se encontró información del año ${anioElegido} en el desgloce.`);
   }
 }
+
